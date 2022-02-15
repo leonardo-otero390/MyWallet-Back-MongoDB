214243878185e5174f9c1098b8951cb55a696c41
Liaess <yann.melo@gmail.com>
2022-02-15 16:16:03 -0300

    Preparando revisão de código

--- src/database/connection.js
+++
@@ -1,8 +0,0 @@
-import '../setup.js';
-import { MongoClient } from 'mongodb';
-
-const mongoClient = new MongoClient(process.env.MONGO_URI);
-mongoClient.connect();
-const db = mongoClient.db(process.env.DB_NAME);
-
-export default db;
--- src/setup.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-
-let envFile = '.env.dev';
-
-if (process.env.NODE_ENV === 'prod') envFile = '.env';
-
-dotenv.config({
-  path: envFile,
-});
